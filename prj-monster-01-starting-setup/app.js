258927f4cb33d76d3fbc87fb21f824e305f3ab03
Gary Ng <garyng@garys-mbp.attlocal.net>
2024-05-17 13:13:45 -0700

    fix import and typo

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -6,19 +6,20 @@ const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
-      montsterHealth: 100,
+      monsterHealth: 100,
     };
   },
   methods: {
     attackMonster() {
       const attackValue = getRandomValue(5, 12);
-      this.montsterHealth -= attackValue;
+      this.monsterHealth -= attackValue;
       this.attackPlayer();
     },
-  },
-  attackPlayer() {
-    const attackValue = getRandomValue(8, 15);
-    this.playerHealth -= attackValue;
+
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+    },
   },
 });
 
