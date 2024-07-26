8ae6a57d5c606e33e019700d15278c300c7ecd90
Gary Ng <garyng@garys-mbp.attlocal.net>
2024-07-26 10:24:03 -0700

    import css from link

--- vue-compostion-api-noteballs/src/App.vue
+++ vue-compostion-api-noteballs/src/App.vue
@@ -1,16 +1,22 @@
-<script setup>
-import { RouterLink } from 'vue-router';
 
-
-</script>
 
 <template>
-  <RouterLink to="/">Notes</RouterLink>
-  <RouterLink to="stats">Stats</RouterLink>
+
+  <NavBar/>
+  <!-- <RouterLink to="/" class="button">Notes</RouterLink> |
+  <RouterLink to="stats" class="button">Stats</RouterLink> -->
 
   <RouterView/>
 </template>
 
-<style scoped>
+<script setup>
+import { RouterLink } from 'vue-router';
+import NavBar from '@/components/Layout/NavBar.vue'
 
+</script>
+
+
+<style scoped>
+/* @import 'bulma/css/bulma.min.css'; */
+@import "https://cdn.jsdelivr.net/npm/bulma@1.0.1/css/bulma.min.css";
 </style>
