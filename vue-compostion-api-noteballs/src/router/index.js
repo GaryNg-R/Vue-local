78015ca2d313f2eb6f8542387e2a565099b5b7fd
Gary Ng <garyng@garys-mbp.attlocal.net>
2024-07-26 09:57:38 -0700

    refactor routes to const inside index.js

--- vue-compostion-api-noteballs/src/router/index.js
+++ vue-compostion-api-noteballs/src/router/index.js
@@ -3,12 +3,14 @@ import App from "@/App.vue";
 import ViewNotes from "@/views/ViewNotes.vue";
 import ViewStats from "@/views/ViewStats.vue";
 
+const routes = [
+  { path: "/", name: "notes", component: ViewNotes },
+  { path: "/stats", name: "stats", component: ViewStats },
+];
+
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: [
-    { path: "/", name: "notes", component: ViewNotes },
-    { path: "/stats", name: "stats", component: ViewStats },
-  ],
+  routes,
 });
 
 export default router;
