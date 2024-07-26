c1099d3ee04fb820fa60bf48cf775e4022514422
Gary Ng <garyng@garys-mbp.attlocal.net>
2024-07-26 13:28:00 -0700

    able to add new notes

--- vue-compostion-api-noteballs/src/views/ViewNotes.vue
+++ vue-compostion-api-noteballs/src/views/ViewNotes.vue
@@ -3,24 +3,31 @@
     <div class="card has-background-success-dark p-4">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note" />
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add a new note"
+          />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">
+          <button
+            @click="addNote"
+            :disabled="!newNote"
+            class="button is-link has-background-success"
+          >
             Add New Note
           </button>
         </div>
       </div>
     </div>
 
-    <div v-for="i in 3" class="card mb-4">
+    <div v-for="note in notes" class="card mb-4" :key="note.id">
       <div class="card-content">
         <div class="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-          iaculis mauris.
+          {{ note.content }}
         </div>
       </div>
       <footer class="card-footer">
@@ -30,3 +37,33 @@
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+const newNote = ref("");
+const notes = ref([
+  {
+    id: "id1",
+    content:
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus neciaculis mauris.",
+  },
+  {
+    id: "id2",
+    content: " This is a second note",
+  },
+]);
+
+const addNote = () => {
+  let currentDate = new Date().getTime(),
+    id = currentDate.toString();
+
+  let note = {
+    id: id,
+    content: newNote.value,
+  };
+  //console.log(note);
+  // notes.value.push(note);
+  notes.value.unshift(note);
+  newNote.value = "";
+};
+</script>
